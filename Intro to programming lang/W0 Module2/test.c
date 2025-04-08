f57d78ded4e47832a5a2186d85a92f1c1ab8c7e4
shihab-7 <shihabshahriar543@gmail.com>
2025-04-09 01:22:04 +0600

    loop module

---
+++ Intro to programming lang/W0 Module2/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a=2, b=3;
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    return 0;
+}
