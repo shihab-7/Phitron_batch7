27688997a714e71da1d2d150eb43f612858cc31b
shihab-7 <shihabshahriar543@gmail.com>
2025-10-05 09:38:22 +0600

    week 5 module 18 merge sort done

---
+++ Intro to basic algorithm/W5 Module18/divide.cpp
@@ -0,0 +1,23 @@
+# include <bits/stdc++.h>
+using namespace std;
+void devide(int a[],int l,int r)
+{
+    for(int i=l;i<=r;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    
+    if(l>=r) return;
+    int mid=(l+r)/2;
+    devide(a,l,mid);
+    devide(a,mid+1,r);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    devide(a,0,n-1);
+    return 0;
+}
