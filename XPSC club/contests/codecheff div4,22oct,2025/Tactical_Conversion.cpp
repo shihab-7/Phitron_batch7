cff912da92a1ae875cb454259a886b9e9cabae66
shihab-7 <shihabshahriar543@gmail.com>
2025-10-23 23:23:54 +0600

    upsolved 5th no in codechef div4 22nd oct

---
+++ XPSC club/contests/codecheff div4,22oct,2025/Tactical_Conversion.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define shihab ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define test int t; cin>>t; while(t--)
+#define endl '\n'
+#define printv(v) for(auto a:v) cout<<a<<" ";
+#define printmp(m) for(auto a:m) cout<<a.first<<" "<<a.second<<endl;
+#define ll long long
+
+int main()
+{
+    shihab
+    test
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int c1=0,len=0,mx_len=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='1')
+            {
+                c1++;
+                len++;
+                mx_len=max(mx_len,len);
+            }
+            else len=0;
+        }
+        if(c1==2&& mx_len==2)cout<<"No"<<endl;
+        else if(c1==3&&mx_len==3)cout<<"No"<<endl;
+        else cout<<"Yes"<<endl;
+    }
+    return 0;
+}
