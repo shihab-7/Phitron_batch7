68cfaa86c4638de795f2f320d469171ddb214827
shihab-7 <shihabshahriar543@gmail.com>
2026-01-25 00:56:06 +0600

    codecheff div4,7jan,2026 upsolved e

--- XPSC club/contests/codecheff div4,7jan,2026/Upward_Path.cpp
+++ XPSC club/contests/codecheff div4,7jan,2026/Upward_Path.cpp
@@ -15,15 +15,63 @@ int main()
     {
         ll n;
         cin>>n;
-        ll a[2][n];
-        for(int i=0;i<2;i++)
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        for(int i=0;i<n;i++) cin>>b[i];
+
+        vector<int>pre(n,-1),suf(n,-1);
+        pre[0]=min(a[0],b[0]);
+        for(int i=1;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                cin>>a[i][j];
-            }
+            int mn=min(a[i],b[i]);
+            int mx=max(a[i],b[i]);
+            if(pre[i-1]<=mn) pre[i]=mn;
+            else if(pre[i-1]<=mx) pre[i]=mx;
+            else break;
         }
         
+        suf[n-1]=max(a[n-1],b[n-1]);
+        for(int i=n-2;i>=0;i--)
+        {
+            int mn=min(a[i],b[i]);
+            int mx=max(a[i],b[i]);
+            if(suf[i+1]>=mx) suf[i]=mx;
+            else if(suf[i+1]>=mn) suf[i]=mn;
+            else break;
+        }
+        // printv(pre);
+        // cout<<endl;
+        // printv(suf);
+        // cout<<endl;
+
+        bool ok=false;
+        for(int i=0;i<n;i++)
+        {
+            int mn=min(a[i],b[i]);
+            int mx=max(a[i],b[i]);
+
+            int smne=0,pche=1e9;
+            bool f=true;
+            if(i-1>=0)
+            {
+                if(pre[i-1]!=-1) smne=pre[i-1];
+                else f=false;
+            }
+            if(i+1<n)
+            {
+                if(suf[i+1]!=-1) pche=suf[i+1];
+                else f=false;
+            }
+
+            if(f==true && smne<=mn && mx<=pche) 
+            {
+                ok=true;
+                break;
+            }
+        }
+
+        if(ok) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;  
     }
     return 0;
 }
